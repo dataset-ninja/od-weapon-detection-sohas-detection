a26fcc13ba44376bdaa685b333b25996cc945836
Alex Erastov <123257559+erastov-alex@users.noreply.github.com>
2023-10-23 16:20:32 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -21,7 +21,7 @@ HIDE_DATASET = True  # set False when 100% sure about repo quality
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_SA_4_0()
-APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Safety(is_used=False)]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Security(is_used=True)]
 CATEGORY: Category = Category.Safety(extra=Category.Surveillance())
 
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
